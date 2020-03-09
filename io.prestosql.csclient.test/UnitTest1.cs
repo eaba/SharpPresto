029d7401a21b2f1761e28af9f94673d9fe663cea
eabanonu <eabanonu@yahoo.com>
2020-03-09 17:00:20 +0100

    Cannot cast Hk2InjectionManagerFactory to InjectionManagerFactory [pulsar type/bug]

--- io.prestosql.csclient.test/UnitTest1.cs
+++ io.prestosql.csclient.test/UnitTest1.cs
@@ -2,37 +2,66 @@ using System;
 using System.Data.Common;
 using io.prestosql.client;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace io.prestosql.csclient.test
 {
     //http://pulsar.apache.org/docs/en/sql-getting-started/
     //https://pulsar.apache.org/docs/en/sql-rest-api/
-    //docker run -it -p 6650:6650 -p 8080:8080 -p 8081:8081 --mount source=pulsardata,target=/pulsar/data --mount source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.5.0 bin/pulsar standalone
+    //docker run -it -p 6650:6650 -p 8080:8080 -p 8081:8081 --mount source=pulsardata,target=/pulsar/data --mount source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.5.0 bin/pulsar standalone sql-worker function-worker
     public class UnitTest1
     {
+        private ITestOutputHelper _helper;
+
+        public UnitTest1(ITestOutputHelper Helper)
+        {
+            _helper = Helper;
+        }
+
         [Fact]
         public void FirstTest()
         {
-            using (DbConnection Conn = new PrestoSqlDbConnection())
+            using DbConnection Conn = new PrestoSqlDbConnection
             {
-                Conn.ConnectionString = "http://localhost:8081";
-                Conn.Open();
+                ConnectionString = "http://localhost:8081"
+            };
+            Conn.Open();
 
-                using (DbCommand Cmd = Conn.CreateCommand())
+            using var Cmd = Conn.CreateCommand();
+            Cmd.CommandText = "show catalogs";
+
+            using var Reader = Cmd.ExecuteReader();
+            if (Reader.Read())
+            {
+                for (var i = 0; i < Reader.FieldCount; i++)
                 {
-                    Cmd.CommandText = "show catalogs";
+                    var T = Reader.GetFieldType(i);
+                    var Value = Reader.GetValue(i);
+                    _helper.WriteLine(Value.ToString());
+                }
+            }
+        }
 
-                    using (DbDataReader Reader = Cmd.ExecuteReader())
-                    {
-                        if (Reader.Read())
-                        {
-                            for (int i = 0; i < Reader.FieldCount; i++)
-                            {
-                                Type T = Reader.GetFieldType(i);
-                                object Value = Reader.GetValue(i);
-                            }
-                        }
-                    }
+        [Fact]
+        public void Schema()
+        {
+            using DbConnection Conn = new PrestoSqlDbConnection
+            {
+                ConnectionString = "http://localhost:8081"
+            };
+            Conn.Open();
+
+            using var Cmd = Conn.CreateCommand();
+            Cmd.CommandText = "show schemas in pulsar";
+
+            using var Reader = Cmd.ExecuteReader();
+            if (Reader.Read())
+            {
+                for (var i = 0; i < Reader.FieldCount; i++)
+                {
+                    var T = Reader.GetFieldType(i);
+                    var Value = Reader.GetValue(i);
+                    _helper.WriteLine(Value.ToString());
                 }
             }
         }
@@ -40,56 +69,48 @@ namespace io.prestosql.csclient.test
         [Fact]
         public void SingleResultTest()
         {
-            using (DbConnection Conn = new PrestoSqlDbConnection())
-            {
-                Conn.ConnectionString = "http://localhost:8080";
-                Conn.Open();
+            using DbConnection Conn = new PrestoSqlDbConnection();
+            Conn.ConnectionString = "http://localhost:8080";
+            Conn.Open();
 
-                using (DbCommand Cmd = Conn.CreateCommand())
-                {
-                    Cmd.CommandText = "SELECT 1";
+            using var Cmd = Conn.CreateCommand();
+            Cmd.CommandText = "SELECT 1";
 
-                    int v = Convert.ToInt32(Cmd.ExecuteScalar());
+            var v = Convert.ToInt32(Cmd.ExecuteScalar());
 
-                    if (v != 1)
-                        Assert.False(false,"Invalid return value");
-                }
-            }
+            if (v != 1)
+                Assert.False(false,"Invalid return value");
         }
 
 
         [Fact]
         public void ErrorTest()
         {
-            using (DbConnection Conn = new PrestoSqlDbConnection())
+            using DbConnection Conn = new PrestoSqlDbConnection();
+            Conn.ConnectionString = "http://localhost:8080";
+            Conn.Open();
+
+            using var Cmd = Conn.CreateCommand();
+            Cmd.CommandText = @"ERROR HERE!";
+
+
+            try
             {
-                Conn.ConnectionString = "http://localhost:8080";
-                Conn.Open();
-
-                using (DbCommand Cmd = Conn.CreateCommand())
+                using (var Reader = Cmd.ExecuteReader())
                 {
-                    Cmd.CommandText = @"ERROR HERE!";
-
-
-                    try
+                    while (Reader.Read())
                     {
-                        using (DbDataReader Reader = Cmd.ExecuteReader())
-                        {
-                            while (Reader.Read())
-                            {
-                                for (int i = 0; i < Reader.FieldCount; i++)
-                                    Console.Write(Reader[i]);
-
-                            }
-                        }
-
-                        Assert.False(false);
-                    }
-                    catch (PrestoSqlException ex)
-                    {
+                        for (var i = 0; i < Reader.FieldCount; i++)
+                            Console.Write(Reader[i]);
 
                     }
                 }
+
+                Assert.False(false);
+            }
+            catch (PrestoSqlException ex)
+            {
+
             }
         }
 
@@ -97,27 +118,21 @@ namespace io.prestosql.csclient.test
         [Fact]
         public void SecondTest()
         {
-            using (DbConnection Conn = new PrestoSqlDbConnection())
-            {
-                Conn.ConnectionString = "http://localhost:8080";
-                Conn.Open();
+            using DbConnection Conn = new PrestoSqlDbConnection();
+            Conn.ConnectionString = "http://localhost:8080";
+            Conn.Open();
 
-                using (DbCommand Cmd = Conn.CreateCommand())
-                {
-                    Cmd.CommandText = @"SELECT orderpriority, SUM(totalprice) AS totalprice
+            using var Cmd = Conn.CreateCommand();
+            Cmd.CommandText = @"SELECT orderpriority, SUM(totalprice) AS totalprice
 FROM tpch.sf1.orders AS O
 INNER JOIN tpch.sf1.customer AS C ON O.custkey = C.custkey 
 GROUP BY orderpriority ORDER BY orderpriority";
 
-                    using (DbDataReader Reader = Cmd.ExecuteReader())
-                    {
-                        while (Reader.Read())
-                        {
-                            for (int i = 0; i < Reader.FieldCount; i++)
-                                Console.Write(Reader[i]);
-                        }
-                    }
-                }
+            using var Reader = Cmd.ExecuteReader();
+            while (Reader.Read())
+            {
+                for (var i = 0; i < Reader.FieldCount; i++)
+                    Console.Write(Reader[i]);
             }
         }
     }
